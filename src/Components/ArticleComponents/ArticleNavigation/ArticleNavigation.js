7fc8d79d30f3c8058d391ad36427b26e19982b2a
Mark Romero <markromero@marks-mbp.lan>
2019-04-29 00:17:21 -0600

    updated footer to functional component

--- src/Components/ArticleComponents/ArticleNavigation/ArticleNavigation.js
+++ src/Components/ArticleComponents/ArticleNavigation/ArticleNavigation.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function scrollToTop() {
     document.body.scrollTop = document.documentElement.scrollTop = 0;
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <div className='nav-bar-logo'>Sub<span>lasa</span></div>
-          <p>
-            <a href="https://marksthought.com">Created by @mromero</a>.
-          </p>
-        </div>
-      </footer>
-    )
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <div className='nav-bar-logo'>Sub<span>lasa</span></div>
+        <p>
+          <a href="https://marksthought.com">Created by @mromero</a>.
+        </p>
+      </div>
+    </footer>
+  )
 }
+
+export default Footer;
